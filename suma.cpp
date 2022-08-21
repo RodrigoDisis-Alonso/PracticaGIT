5a46ab8835d8124e67395a769d96423f7a5cdac0
RodrigoDisis-Alonso <111076092+RodrigoDisis-Alonso@users.noreply.github.com>
2022-08-21 13:03:31 -0500

    Se le hizo una modificacion al codigo de suma

--- suma.cpp
+++ suma.cpp
@@ -1,13 +1,15 @@
 #include <iostream.h>
 #include <conio.h>
 main()
-{int a,b,suma;
+{int a,b,suma multi;
 cout<<"\n ingrese numero a:";
 cin>>a;
 cout<<"\n ingrese numero b:";
 cin>>b;
 suma=a+b;
+multi=a*b;
 cout<<"\n la suma de "<<a<<"+"<<b<<" es:"<<suma;
+cout<<"\n la multiplicacion de "<<a<<"x"<<b<<" es:"<<multi;
 getch();
 return 0;
 }
